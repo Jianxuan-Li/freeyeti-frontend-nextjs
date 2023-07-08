8a9f066566e11d67ba070f02445d617a441e21e5
Jianxuan Li <liujin834@gmail.com>
2023-07-08 16:20:01 -0400

    feat: new blog body

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -11,11 +11,15 @@ export default async function BlogPage(context) {
   const { id } = result.items[0];
   const blog = await getBlogById(id);
 
+  if (!blog) {
+    return <div>Blog not found</div>;
+  }
+
   return (
     <main className={styles.blogBody}>
       <h1 className={styles.blogPageTitle}>{blog.title}</h1>
       <p className={styles.blogPageDate}>{moment(blog.meta.first_published_at).format("Y-M-D")}</p>
-      <div dangerouslySetInnerHTML={{ __html: blog.body }} />
+      <div dangerouslySetInnerHTML={{ __html: blog.context }} />
     </main>
   );
 }
