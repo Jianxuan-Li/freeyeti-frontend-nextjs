57d214541d4a36363783065effa7edb0a8aff1df
Jianxuan Li <liujin834@gmail.com>
2023-07-09 22:56:21 -0400

    feat: background animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,17 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
-import { getBlogs, getHomepageBg } from "@/modules/blog/requests";
+import { getBlogs } from "@/modules/blog/requests";
 import Link from "next/link";
+import IndexBg from "@/components/IndexBackground"
 
 const inter = Inter({ subsets: ["latin"] });
 
-async function getData() {
-  return await getBlogs();
-}
-
 export default async function Home() {
-  const { items, meta } = await getData();
-
-  const images = await getHomepageBg();
-  let bg = [];
-  if (images && images.meta && images.meta.total_count > 0) {
-    bg = images.items.map((item) => {
-      return item.meta.download_url;
-    });
-  }
-
-  let style = {}
-
-  if (bg.length > 0) {
-    style = {
-      backgroundImage: `url(${bg[0]})`,
-    }
-  }
+  const { items, meta } = await getBlogs();
 
   return (
     <main className={styles.main}>
-      <div className={styles.bg} style={style}></div>
+      <IndexBg />
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Yeti&apos;s blog
       </h1>
---
+++ src/components/IndexBackground/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from 'react'
+import { getHomepageBg } from "@/modules/blog/requests";
+import styles from "./page.module.css";
+
+type Props = {}
+let timer: any = null;
+let currIndex = 0;
+
+export default function index({ }: Props) {
+  const [curr, setCurr] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    getHomepageBg().then((images) => {
+      if (!images || !images.meta || !images.meta.total_count) return;
+
+      const bg = images.items.map((item) => {
+        return item.meta.download_url;
+      });
+      setCurr(bg[0]);
+      currIndex = 0;
+
+      timer = setInterval(() => {
+        if (currIndex === bg.length - 1) {
+          currIndex = 0;
+          setCurr(bg[0]);
+          return;
+        }
+        currIndex++;
+        setCurr(bg[currIndex]);
+      }, 7000);
+    })
+
+    return () => {
+      if (timer) clearInterval(timer);
+    }
+
+  }, []);
+
+  if (!curr) return null;
+  return (
+    <div className={styles.bg} style={{ backgroundImage: `url(${curr})` }}></div>
+  )
+}
