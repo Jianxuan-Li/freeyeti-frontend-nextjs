ce6627ee11652cc9aa1a4e2398a453c9f95660ed
Jianxuan Li <liujin834@gmail.com>
2023-07-12 22:20:27 -0400

    feat: add clarity tag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,19 @@
+/* tslint:disable */
+
 export default function Head() {
   return (
     <>
       <title>Yeti&#39;s Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="A boring blog" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/yeti-white-o.png" />
+      <script type="text/javascript">
+        {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hyfnmdtmz6");`}
+      </script>
     </>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <link rel="icon" type="image/png" href="/yeti-white-o.png" />
       <head />
       <body>{children}</body>
     </html>
