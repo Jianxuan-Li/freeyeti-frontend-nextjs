53a6e715267d4f3cc59e49593418f5de6908d51f
Jack Li <liujin834@gmail.com>
2023-11-06 14:07:15 -0500

    optimize: remove useless functions

---
+++ src/utils/request.ts
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie';
+
+const CSRF_COOKIE_NAME = 'csrftoken';
+const CSRF_HEADER_NAME = 'X-CSRFTOKEN';
+
+export const get = async (url: string, params: any) => {
+  if (Object.keys(params).length) {
+    url += '?' + new URLSearchParams(params);
+  }
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      [CSRF_HEADER_NAME]: Cookies.get(CSRF_COOKIE_NAME)
+    },
+    cache: 'no-store'
+  });
+  return response.json();
+};
