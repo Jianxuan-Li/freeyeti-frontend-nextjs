8ba77e917784e5c27e56b11f1560c10ff5e697da
Jianxuan Li <liujin834@gmail.com>
2024-05-03 21:45:04 -0400

    fix: hall

--- src/components/Chat/Hall/index.tsx
+++ src/components/Chat/Hall/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { AuthContext } from '@/context/AuthContext';
 import { logout } from '@/modules/auth/logout';
-import Contacts from './Contacts';
+import RoomCard from './RoomCard';
+import NewRoom from './NewRoom';
+import { getChatRooms } from '@/modules/chat/requests';
+import { Room } from '@/modules/chat/types';
 
 type Props = {};
 
+/*
+{
+    "id": 1,
+    "name": "gg",
+    "slug": "ee",
+    "user": {
+        "id": 1,
+        "first_name": "Jack",
+        "last_name": "Li"
+    },
+    "is_private": false,
+    "created_at": "2023-11-04T21:35:30.795825Z",
+    "updated_at": "2023-11-04T21:35:30.795860Z"
+}
+*/
+
 export default function Hall({}: Props) {
   const { user, setUser } = React.useContext(AuthContext);
+  const [rooms, setRooms] = React.useState<Room[]>([]);
+
+  React.useEffect(() => {
+    getChatRooms().then((res) => {
+      setRooms(res.data.data);
+    });
+  }, []);
 
   const onRoomCreated = () => {
     getChatRooms().then((res) => {
